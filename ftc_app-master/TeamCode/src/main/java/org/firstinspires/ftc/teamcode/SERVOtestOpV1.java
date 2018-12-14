f304e9ca7d603ee1fef3eaf3bca30e630d90e789
Jaden Forde <jmforde19@denver.k12.ia.us>
2018-12-13 18:39:09 -0600

    added more tensor flow autonomous

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SERVOtestOpV1.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SERVOtestOpV1.java
@@ -18,6 +18,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 */
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -37,7 +38,7 @@ import java.util.Arrays;
  * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
  */
 @TeleOp(name="SERVOtestOpV1", group="TeleOp")
-
+//@Disabled
 public class SERVOtestOpV1 extends OpMode {
     /* Declare OpMode members. */
     private Gyroscope imu;
